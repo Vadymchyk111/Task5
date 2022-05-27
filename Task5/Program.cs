4a9403e52b9a4356252a940a00f337ecf5a3d87d
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 13:28:09 +0300

    AddCode

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -10,6 +10,15 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            string name = "Петров";
+            string surname = "Петр";
+            string temp;
+            Console.WriteLine(name + " "+ surname);
+            temp = name;
+            name = surname;
+            surname = temp;
+            temp = null;
+            Console.WriteLine(name + " " + surname);
         }
     }
 }
